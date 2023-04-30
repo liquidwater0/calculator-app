2e5ae2f4a8ce40daef5f2ecf3c2acf54cb37b7ab
liquidwater0 <32186774+liquidwater0@users.noreply.github.com>
2023-04-29 20:06:33 -0500

    initial commit!.!.

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+import "./scss/App.scss";
+
+function App() {
+	return (
+		<>
+			app
+			
+			{/* <div className="attribution">
+				Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+				Coded by <a href="#">Your Name Here</a>.
+			</div> */}
+		</>
+	)
+}
+
+export default App;
